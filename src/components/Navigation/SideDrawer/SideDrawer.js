4bf88c4c457e43b385c5cb7e502a297a6e8b859e
Barna Biro <barna.biro@lateral-inc.com>
2020-06-11 14:32:55 +0300

    finishing up the components

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
+
+const sideDrawer = (props) => {
+  let attachedClasses = props.open ? [classes.SideDrawer, classes.Open] : [classes.SideDrawer, classes.Closed];
+
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo height='11%' />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;
